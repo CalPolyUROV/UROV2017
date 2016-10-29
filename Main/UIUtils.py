dbaba477bc22aa50d54972abb3511a9a92ef5d01
LMPS97 <luca_mps@hotmail.com>
2016-10-29 12:32:44 -0700

    Moved Pygame funcs and Objs to raper class
    Functions and objects from pygame were moved to UIUtils.py file to make
    things more simplyfied and more objected oriented

---
+++ Main/UIUtils.py
@@ -0,0 +1,43 @@
+import pygame
+from pygame.locals import *
+
+class UI:
+    def __init__(self):
+        pygame.init()                                           #initializes the UI
+        self.screen = pygame.display.set_mode((1000, 500))
+        pygame.display.set_caption("Cal Poly Control Center")
+
+        self.background = pygame.Surface(self.screen.get_size())          # Set background to white.
+        self.background = self.background.convert()
+        self.background.fill((255, 255, 255))
+
+    def update(self):
+        pygame.display.update()
+
+    def textwrite(self, Positionx, Positiony, Text, r = 10, g = 10 , b = 10, size = 25): #Function that write on screen Strings.
+
+        font = pygame.font.Font(None, size)
+        text = font.render(Text, 0, (r, g, b))
+        textpos = text.get_rect()
+        textpos.centerx = Positionx
+        textpos.centery = Positiony
+        self.background.blit(text, textpos)
+        self.screen.blit(self.background, (0, 0))
+
+    def textdelete(self, Positionx, Positiony, Text, size = 25):                 #Function that deletes strings
+
+        font = pygame.font.Font(None, size)
+        text = font.render(Text, 0, (255, 255, 255))
+        textpos = text.get_rect()
+        textpos.centerx = Positionx
+        textpos.centery = Positiony
+        self.background.blit(text, textpos)
+        self.screen.blit(self.background, (0, 0))
+
+    def blit(self, obj, objPos):
+
+        self.background.blit(obj, objPos)
+
+    def drawRect(self, rect, color = (10, 10, 10), width = 0):
+
+        pygame.draw.rect(self.background, color, rect, width)
