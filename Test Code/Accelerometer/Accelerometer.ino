80897a873b28cb716cdce4a73287b99fd83365ae
sfshaw <sfshaw@calpoly.edu>
2016-11-12 12:15:09 -0800

    Commnet code

--- Test Code/Accelerometer/Accelerometer.ino
+++ Test Code/Accelerometer/Accelerometer.ino
@@ -3,12 +3,14 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
+//create the instance of the sensor
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Testing Orientation Sensor");
+  Serial.println("Testing Orientation and Acceleration Sensor");
   Serial.println("");
+  //open serial
 
   //initialize the sensor
   if (!bno.begin())
@@ -18,12 +20,13 @@ void setup() {
   }
   delay(1000);
 
+  //use for better low power timing?
   bno.setExtCrystalUse(true);
 }
 
 void loop() {
 
-  //get the event
+  //get the orrientation sensor event
   sensors_event_t event;
   bno.getEvent(&event);
 
@@ -34,6 +37,15 @@ void loop() {
   Serial.print(event.orientation.y, 4);
   Serial.print("\tRoll: ");
   Serial.print(event.orientation.z, 4);
+  //  //display the data
+  //  Serial.print("Yaw: ");
+  //  Serial.print(event.orientation.x, 4);
+  //  Serial.print("\tPitch: ");
+  //  Serial.print(event.orientation.y, 4);
+  //  Serial.print("\tRoll: ");
+  //  Serial.print(event.orientation.z, 4);
+
+  //next line
   Serial.println("");
 
 }
